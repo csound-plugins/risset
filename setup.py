4f138d58a0137242e9999c6ed85771e6d7307d2d
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-04-29 09:50:24 +0200

    better version handling

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
+import subprocess
+import sys
 
-long_description = """
-# Csound Package Manager
-
-
-"""
+long_description = open("README.md").read()
+version = subprocess.getoutput("python3 ./risset --version")
 
 setup(
     name = "risset",
-    version = "0.0.1",
+    version = version,
     description = "A package manager for csound",
     author = "Eduardo Moguillansky",
     long_description = long_description,
