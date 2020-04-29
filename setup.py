7f68625be0110130ef100d38b6d5fd81b6eab7eb
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2020-04-29 11:49:29 +0200

    fixed type warnings

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ from setuptools import setup
 import subprocess
 import sys
 
+if (sys.version_info.major, sys.version_info.minor) < (3, 6):
+    print("Python >= 3.6 required")
+    sys.exit(-1)
+
 long_description = open("README.md").read()
 version = subprocess.getoutput("python3 ./risset --version")
 
 setup(
     name = "risset",
     version = version,
+    python_requires = ">=3.6",
     description = "A package manager for csound",
     author = "Eduardo Moguillansky",
+    author_email = "eduardo.moguillansky@gmail.com",
     long_description = long_description,
-    long_description_content_type='text/markdown',
-    scripts=['risset']    
+    long_description_content_type = 'text/markdown',
+    py_modules=["risset"],
+    scripts=["risset"],
+    url="https://github.com/csound-plugins/risset"
 )
