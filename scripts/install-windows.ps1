b19286d28a8a8be27eaa9d15d66c2bd6a1f17f73
gesellkammer <eduardo.moguillansky@gmail.com>
2024-12-11 11:22:30 +0100

    add scripts

---
+++ scripts/install-windows.ps1
@@ -0,0 +1,2 @@
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+uv tools install risset
---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv tool install --upgrade risset
+
